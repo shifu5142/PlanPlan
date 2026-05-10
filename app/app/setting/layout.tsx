49aa9ce091632b1f5b45c9049fc6c7d106d5accd
כניסה עמדת שומר     Knisa Security <Knisasec@sviva.gov.il>
2026-05-10 09:56:39 +0300

    remove all files

--- app/app/setting/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react'
-import { SettingsShell } from './_components/settings-shell'
-
-export default function SettingLayout({ children }: { children: ReactNode }) {
-  return <SettingsShell>{children}</SettingsShell>
-}
--- app/app/setting/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function SettingIndexPage() {
-  redirect('/app/setting/profile')
-}
